87dfc1b5d281b39736b84e055b73d12f92818e40
Jax McKinney <mck20018@byui.edu>
2022-04-25 13:01:45 -0600

    Finished

--- Program.cs
+++ Program.cs
@@ -7,7 +7,54 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            //string input = "yes";
+
+            //while (input == "yes")
+            //{
+                //Console.Write("Do you want to continue? ");
+                //input = Console.ReadLine();
+            //}
+
+            //string inputTwo;
+
+            //do
+            //{
+                //Console.Write("Do you want to continue? ");
+                //inputTwo = Console.ReadLine();
+            //} while (inputTwo == "yes");
+
+            //for (int i = 0; i < 10; i++){Console.WriteLine(i);}
+
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,11);
+            Console.Write(number);
+            
+            int guess = -1;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            
+
+            
+
         }
     }
 }
